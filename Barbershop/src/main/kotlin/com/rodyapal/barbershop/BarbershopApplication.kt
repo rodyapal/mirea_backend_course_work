8ec06e5b83840e987d0313f0ddf2f31d540b8cc5
Rodion Sviridov <sviridov.rod@yandex.ru>
2022-11-26 13:37:33 +0300

    Init commit

---
+++ Barbershop/src/main/kotlin/com/rodyapal/barbershop/BarbershopApplication.kt
@@ -0,0 +1,11 @@
+package com.rodyapal.barbershop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BarbershopApplication
+
+fun main(args: Array<String>) {
+	runApplication<BarbershopApplication>(*args)
+}
